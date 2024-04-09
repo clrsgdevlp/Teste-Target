335ab05f2f2613cf2f766a3237f8934ade987482
claiton <claitonrsg@gmail.com>
2024-04-08 22:30:44 -0300

    projeto iniciado

---
+++ Fibonacci/CaculaFibonacci.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class CaculaFibonacci
+    {
+        public void VerificaFibonacci(int numero)
+        {
+            if (numero == 0 || numero == 1)
+            {
+                ImprimeResultado(true,numero);
+                return;
+            }
+
+            int anterior = 0;
+            int atual = 1;
+
+            while (atual < numero)
+            {
+                int proximoNumero = anterior + atual;
+                anterior = atual;
+                atual = proximoNumero;
+            }
+
+            if (atual == numero)
+            {
+                ImprimeResultado(true, numero);
+                return;
+            }
+         
+            ImprimeResultado(false,numero);
+        }
+
+        private void ImprimeResultado(bool EhFibonacci, int numero)
+        {
+            if (EhFibonacci)
+            {
+                Console.WriteLine($"{numero} pertence à sequência!");
+                return;
+            }
+
+            Console.WriteLine($"{numero} não pertence a sequencia!");
+        }
+    }
+}
+
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Fibonacci
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um número para saber se pertence a sequancia fibonacci");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            var calculoFibonacci = new CaculaFibonacci();
+
+            calculoFibonacci.VerificaFibonacci(numero);
+
+           
+        }
+    }
+}
